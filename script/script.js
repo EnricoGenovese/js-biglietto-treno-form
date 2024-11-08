d478f65e8df9d6b0526a668719a03b4647d7f911
EnricoGenovese <enricogeno@libero.it>
2024-11-08 17:32:00 +0100

    add passenger info variables; add final ticket print with passenger informations and ticket price; changed colors in css;

--- script/script.js
+++ script/script.js
@@ -19,18 +19,28 @@ console.clear();
 // realizziamo le specifiche scritte sopra. 
 // La risposta finale (o output) sarà anch’essa da scrivere in console.
 
+// MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante 
+// allora realizzeremo un form in pagina in cui l’utente potrà inserire 
+// i dati e visualizzare il calcolo finale con il prezzo.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina 
+// (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+//  Questo richiederà un minimo di ricerca.
 
 // 1 - Programma che chiede distanza in chilometri ed età al passeggero
 
 
-const distance = document.getElementById("distance");
-const age = document.getElementById("age");
 
-const ticket = document.getElementById("ticket");
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function(event) { 
+form.addEventListener("submit", function (event) {
+    const distance = document.getElementById("distance");
+    const age = document.getElementById("age");
+    //
+    const pName = document.getElementById("name");
+    const pSurn = document.getElementById("surname");
+    //
     event.preventDefault();
     //
     const pricePerKm = 0.21;
@@ -38,7 +48,7 @@ form.addEventListener("submit", function(event) {
     let ticketKm = parseInt(distance.value);
     let ticketPrice = ticketKm * pricePerKm;
     //
-    const maxJuniorAge = 17;  
+    const maxJuniorAge = 17;
     const minSeniorAge = 65;
     //
     const juniorDiscount = 20;
@@ -47,13 +57,23 @@ form.addEventListener("submit", function(event) {
     let userAge = parseInt(age.value);
     //
     if (!isNaN(userAge) && !isNaN(ticketKm)) {
-         if (userAge <= maxJuniorAge) {
+        if (userAge <= maxJuniorAge) {
             ticketPrice -= (ticketPrice * juniorDiscount) / 100;
         } else if (userAge >= minSeniorAge) {
             ticketPrice -= (ticketPrice * seniorDiscount) / 100;
         } else {
             ticketPrice;
-        }  
+        }
     }
-    ticket.innerHTML = ticketPrice.toFixed(2) + "€";
+
+    firstname.innerHTML = `Nome del passeggero: <strong>${pName.value}</strong>`;
+    lastname.innerHTML = `Cognome del passeggero: <strong>${pSurn.value}</strong>`;
+    price.innerHTML = `Il prezzo del biglietto è di: <strong>${ticketPrice.toFixed(2) + "€"}</strong>`;
+    message.innerHTML = "Grazie per aver viaggiato con noi! Arrivederci";
+
+    this.reset()
 })
+
+
+
+
